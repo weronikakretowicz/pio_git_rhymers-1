155e011b637c1df0c1abc21f2929cff67e20a743
Weronika Kretowicz <weronikakretowicz@wp.pl>
2022-04-25 00:05:55 +0200

    Fix methods names

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -1,15 +1,15 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 	public interface Rhymersfactory {
-	
-		public defaultCountingOutRhymer GetStandardRhymer();
 		
-		public defaultCountingOutRhymer GetFalseRhymer();
+		public DefaultCountingOutRhymer getStandardRhymer();
 		
-		public defaultCountingOutRhymer GetFIFORhymer();
+		public DefaultCountingOutRhymer getFalseRhymer();
 		
-		public defaultCountingOutRhymer GetHanoiRhymer();
+		public DefaultCountingOutRhymer getFIFORhymer();
+		
+		public DefaultCountingOutRhymer getHanoiRhymer();
 		
 	}
