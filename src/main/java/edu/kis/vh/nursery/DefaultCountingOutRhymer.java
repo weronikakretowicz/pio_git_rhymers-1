150b47baa9e05c10f3630825f35845aa504ae282
vor-o-na <78507829+vor-o-na@users.noreply.github.com>
2022-05-15 21:08:11 +0200

    stale_i_annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+    public static final int MAX_LENGTH = 12;
+    public static final int EMPTY_VALUE = -1;
+    public static final int MAX_INDEX = 11;
+    private final int[] numbers = new int[MAX_LENGTH];
 
-	public int total = -1;
+    public int total = EMPTY_VALUE;
 
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
-	}
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return numbers[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return numbers[total--];
-					}
+    public boolean callCheck() {
+        return total == EMPTY_VALUE;
+    }
+
+    public boolean isFull() {
+        return total == MAX_INDEX;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return EMPTY_VALUE;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return EMPTY_VALUE;
+        return numbers[total--];
+    }
 
 }
